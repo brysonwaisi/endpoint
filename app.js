248482e702253f2f8351e1ac79a15a0f376ebd34
brysonwaisi <brysonnyamwange@gmail.com>
2023-09-08 18:06:02 +0300

    Initial Commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 function getSlackName(req) {
-  return req.query.slack_name || "Unknown";
+  return req.query.slack_name || "nyamwange";
 }
 
 function getCurrentDay() {
@@ -21,18 +21,19 @@ function getCurrentDay() {
 
 function getCurrentUtcTime() {
   const now = new Date();
-  return now.toISOString();
+  return now.toISOString().split(".")[0] + "Z";
 }
 
 app.get("/api", (req, res) => {
   const slack_name = getSlackName(req);
   const current_day = getCurrentDay();
   const utc_time = getCurrentUtcTime();
-  const track = req.query.track || "Unknown";
+  const track = req.query.track || "backend";
 
-  const github_file_url = "";
+  const github_file_url =
+    "https://github.com/brysonwaisi/endpoint/blob/main/app.js";
 
-  const github_repo_url = "";
+  const github_repo_url = "https://github.com/brysonwaisi/endpoint";
 
   const response_data = {
     slack_name,
